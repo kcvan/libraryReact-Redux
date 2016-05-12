98685c1e1bf800d8796233cb6b1f923c2ade97d5
kcvan <kcongvan@gmail.com>
2016-05-12 12:09:54 -0700

    Add return statement that returns a type and payload to our actions index

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,10 @@
 export function selectBook(book) {
-  console.log("A book has been selected: ", book.title);
+  // selectBook is an ActionCreator, it needs to return an action,
+  // an object with a type property(usually all uppercase) and a 
+  // payload(optional). The type describes the action. The payload 
+  // is the item which the action is being acted upon.
+  return {
+    type: "BOOK_SELECTED",
+    payload: book
+  };
 };
