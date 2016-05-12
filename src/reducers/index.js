4ba94d5f5f6e716c3f3f593161ef5138c8ca96f8
kcvan <kcongvan@gmail.com>
2016-05-12 15:00:06 -0700

    Add reducer active function that can handle a BOOK_SELECTED payload and linked it to index.js in reducers folder

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,12 @@
 import { combineReducers } from 'redux';
 import BooksReducer from "./reducer_books";
+import ActiveBookReducer from "./reducer_active_book";
 
-// combineReducers is the mapping of our state. 
+// combineReducers is the mapping of our state. These end up as keys in our global
+// state that we can reference. 
 const rootReducer = combineReducers({
   books: BooksReducer
+  activeBook: ActiveBook
 });
 
 export default rootReducer;
